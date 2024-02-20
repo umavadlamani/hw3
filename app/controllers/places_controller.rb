dc6ca0fc086b9b66aac75d88e1edea70488a7df1
uma vadlamani <umasai.vadlamani@kellogg.northwestern.edu>
2024-02-20 20:52:38 +0000

    Places test run

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,19 @@
+class PlacesController < ApplicationController
+
+  def index
+    @places = Place.all
+    render :template => "places/index"
+  end
+
+  def new
+    @place = Place.new
+  end
+
+def create
+    @place = Place.new
+    @place["name"] = params["name"] 
+    @place.save
+    redirect_to "/places"
+  end
+
+end
